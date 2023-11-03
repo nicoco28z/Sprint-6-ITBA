79e23ad8c100e42ca805319f2bcb55ee8a65b0c4
Aldo <aldoandres045@gmail.com>
2023-11-03 13:33:42 -0300

    Agrego tarjetas, direcciones, tipo_cliente y cuenta

--- consultas.sql
+++ consultas.sql
@@ -1,19 +1,61 @@
-DROP DATABASE IF EXISTS itbadb;
-CREATE DATABASE itbabd;
-USE itbabd;
-
-CREATE TABLE sucursal(
-	id INT primary key,
-    nombre CHAR(50) not null,
-    direccion VARCHAR(200) not null
+CREATE TABLE cuentas(
+	nroCuenta INT primary key,
+    tipo VARCHAR(30) not null,
+    idCliente INT not null,
+    saldo FLOAT not null,
+    moneda ENUM("Peso", "Dolar") not null,
+    foreign key (idCliente) references cliente(customer_id),
+    foreign key (tipo) references tipo_cuentas(tipo)
 );
 
-CREATE TABLE cliente(
+CREATE TABLE tipo_cuentas(
+	tipo VARCHAR(30) primary key
+);
+
+CREATE TABLE tipo_classic(
 	id INT primary key,
-    #En el enunciado indica que el DNI es de 10 caracteres
-    dni CHAR(10) not null,
-    nombre CHAR(30) not null,
-    apellido CHAR(30) not null,
-    banco_id INT not null,
-    foreign key (banco_id) references sucursal(id)
+	tarjeta_debito INT,
+	retiros_realizados INT,
+	chequera INT,
+	tipo CHAR(10)
+);
+
+CREATE TABLE tipo_gold(
+	id INT primary key,
+	tarjeta_debito INT,
+	retiros_realizados INT,
+	chequera INT,
+	tipo CHAR(10)
+);
+
+CREATE TABLE tipo_black(
+	id INT primary key,
+	tarjeta_debito INT,
+	retiros_realizados INT,
+	chequera INT,
+	tipo CHAR(10)
+);
+
+CREATE TABLE bancos(
+	nombre VARCHAR(25) primary key
+);
+
+CREATE TABLE tarjetas(
+	nroTarjeta INT primary key,
+	cvv INT not null,
+    fechaVencimiento DATE not null,
+    fechaEmision DATE not null,
+    idCliente INT not null,
+    banco VARCHAR(25) not null,
+    tipo ENUM("Credito", "Debito") not null,
+    foreign key (idCliente) references cliente(customer_id),
+    foreign key (banco) references bancos(nombre)
+);
+
+CREATE TABLE direcciones(
+	provincia VARCHAR(50),
+    ciudad VARCHAR(50),
+    calle VARCHAR(50),
+    altura INT,
+    primary key (provincia, ciudad, calle, altura)
 )
