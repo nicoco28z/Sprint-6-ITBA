88c0615156d43f3e4aa3b2e45b3ca32d8d4f0907
Aldo <aldoandres045@gmail.com>
2023-11-11 13:49:39 -0300

    Trigger corregido, agregados ultimos cambios

--- Problematica_4.sql
+++ Problematica_4.sql
@@ -30,6 +30,55 @@ FULL OUTER JOIN
        GROUP BY branch_id) as cons_debito
 ON Sucursal_2 = Sucursal_1
 
+
+--Obtener el promedio de créditos otorgado por sucursal
+SELECT sucursal.branch_name AS sucursales, AVG(prestamo.loan_total) AS promedio_creditos
+FROM prestamo
+INNER JOIN cliente ON prestamo.customer_id = cliente.customer_id
+INNER JOIN sucursal ON cliente.branch_id = sucursal.branch_id
+GROUP BY sucursal.branch_name;
+
+--TRIGGER
+CREATE TABLE "auditoria_cuenta" (
+	"old_id"	INTEGER,
+	"new_id"	INTEGER,
+	"old_balance"	NUMERIC,
+	"new_balance"	NUMERIC NOT NULL,
+	"old_iban"	TEXT NOT NULL,
+	"new_iban"	TEXT NOT NULL,
+	"old_tipo_cuenta"	INTEGER NOT NULL,
+	"new_tipo_cuenta"	INTEGER NOT NULL,
+	"user_action"	INTEGER NOT NULL,
+	"created_at"	DATETIME NOT NULL
+);
+
+DROP TRIGGER IF EXISTS CUENTAS_BU;
+CREATE TRIGGER CUENTAS_BU
+BEFORE UPDATE ON cuenta
+FOR EACH ROW
+WHEN NEW.balance != OLD.balance OR OLD.account_id != NEW.account_id OR NEW.iban != OLD.iban OR NEW.tipo_cuenta != OLD.tipo_cuenta
+BEGIN
+    INSERT INTO auditoria_cuenta (old_id, new_id, old_balance, new_balance, old_iban, 
+new_iban, old_tipo_cuenta, new_tipo_cuenta, user_action, created_at )
+	 VALUES (
+	    OLD.account_id,
+        NEW.account_id,
+        OLD.balance,
+        NEW.balance,
+        OLD.iban,
+        NEW.iban,
+        OLD.tipo_cuenta,
+        NEW.tipo_cuenta,
+        'Update',
+       	DATE("now")
+    );
+END;
+
+UPDATE cuenta 
+SET balance = balance - 100
+WHERE account_id IN (10, 11, 12, 13, 14);
+
+
 --Creamos un índice para hacer más eficiente la busqueda de clientes a traves del DNI
 CREATE INDEX IF NOT EXISTS busqueda_dni on cliente (customer_dni);
 
@@ -51,44 +100,6 @@ WHERE account_id = 200;
 
 UPDATE cuenta SET balance = (balance + 1000)
 WHERE account_id = 400;
-
---TIGER SDDDDDDDDD|1D            DR--
-CREATE TABLE "auditoria_cuenta" (
-	"old_id"	INTEGER,
-	"new_id"	INTEGER,
-	"old_balance"	NUMERIC,
-	"new_balance"	NUMERIC NOT NULL,
-	"old_iban"	TEXT NOT NULL,
-	"new_iban"	TEXT NOT NULL,
-	"old_tipo_cuenta"	INTEGER NOT NULL,
-	"new_tipo_cuenta"	INTEGER NOT NULL,
-	"user_action"	INTEGER NOT NULL,
-	"created_at"	INTEGER NOT NULL
-);
-
-CREATE TRIGGER CUENTAS_BU
-BEFORE UPDATE ON cuenta
-FOR EACH ROW
-WHEN NEW.balance != OLD.balance OR NEW.iban != OLD.iban OR NEW.tipo_cuenta != OLD.tipo_cuenta
-BEGIN
-    INSERT INTO auditoria_cuenta ( old_id, new_id, old_balance, new_balance, old_iban, 
-new_iban, old_tipo_cuenta, new_tipo_cuenta, user_action )
-	 VALUES (
-	    OLD.id,
-        NEW.id,
-        OLD.balance,
-        NEW.balance,
-        OLD.iban,
-        NEW.iban,
-        OLD.tipo_cuenta,
-        NEW.tipo_cuenta,
-        'Update'
-    );
-END;
-
-UPDATE cuenta 
-SET balance = balance - 100
-WHERE account_id IN (10, 11, 12, 13, 14);
 
 --Registrar el movimiento en la tabla movimientos
 INSERT INTO movimientos(nro_cuenta, monto, tipo_operacion, hora)
